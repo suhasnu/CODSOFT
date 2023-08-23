a245b1d2240bb688d008973d8c451c9150f683b6
Suhas NU <118452352+suhasnu@users.noreply.github.com>
2023-08-23 13:35:43 +0530

    Add files via upload

---
+++ Landing_Page/script.js
@@ -0,0 +1,12 @@
+let darkmode = document.querySelectorAll('#darkmode');
+
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('color');
+    }
+    else{
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('color');
+    }
+}
